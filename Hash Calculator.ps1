40d106e64a71ef4996ae799ea8c39f632d02d6c4
Tainted06 <skateon81@gmail.com>
2021-10-28 18:43:12 +0300

    Switching to Powershell from python

--- Hash Calculator.ps1
+++ Hash Calculator.ps1
@@ -1,5 +1,7 @@
 import tkinter as tk
+from tkinter import *
 
+# Window
 window = tk.Tk()
 window.title('Python Hash Calculator Made By Tainted | https://github.com/Tainted06/Hash-Calculator')
 window_width = 700
@@ -10,5 +12,14 @@ center_x = int(screen_width/2 - window_width / 2)
 center_y = int(screen_height/2 - window_height / 2)
 window.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 window.resizable(False, False)
+# Window
+
+# Text Boxes and Buttons
+text1 = Text(window, width=75, height=1, yscrollcommand=set())
+text1.insert(INSERT, "File Location...")
+text1.pack(padx=1, side=tk.LEFT, pady=130)
+
+# Text Boxes and Buttons
+
 
 window.mainloop()
